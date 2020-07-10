2ddccc28076c0e37e669fbf120f13e4d0797d1a1
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2020-07-10 23:38:45 +0600

    added homework 2

---
+++ hw2/Jacobi.py
@@ -0,0 +1,52 @@
+import numpy as np
+import time
+import math
+
+n = int(input())
+def jacobi (A , f , x ):
+    xnew = n * [0]
+    for i in range(n):
+        s = 0
+        for j in range(0, i):
+            s = s + A [ i ][ j ] * x [ j ]
+        for j in range(i + 1, n):
+            s = s + A [ i ][ j ] * x [ j ]
+        xnew[ i ] = ( f [ i ] - s ) / A [ i ][ i ]
+    return xnew
+
+def diff(x, xnew):
+    s = float(0)
+    for i in range(n):
+        s += float((x[i] - xnew[i]) ** 2) 
+    return float(math.sqrt(s))
+
+def solve (A , f):
+    eps = 0.01
+    xnew = np.zeros(n)
+    while True:
+        x = xnew
+        xnew = jacobi (A , f , x )
+        if ( diff(x, xnew ) < eps ):
+            return x
+
+
+A = np.random.rand(n, n)
+f = np.random.rand(n)
+for i in range(n):
+    for j in range(n):
+        if i != j:
+            A[i][i] += A[i][j]
+print("\nMy algorithm:\n")
+my_time = time.time()
+solve(A, f)
+my_time = time.time() - my_time
+for el in solve(A, f):
+    print(el)
+linalg_time = time.time()
+print("\nLinalg:\n")
+for el in np.linalg.solve(A, f):
+    print(el)
+linalg_time = time.time() - linalg_time
+
+print("\nMy time:", my_time)
+print("Linalg time:", linalg_time)
---
+++ hw2/Seidel.py
@@ -0,0 +1,52 @@
+import time
+import numpy as np
+import math
+
+n = int(input())
+def seidel (A , f , x ):
+    xnew = np.zeros(n)
+    for i in range(n):
+        s = float(0)
+        for j in range(i):
+            s = float(s + A [ i ][ j ] * xnew [ j ])
+        for j in range(i + 1, n):
+            s = float(s + A[ i ][ j ] * x[ j ])
+        xnew[ i ] = float(( f[ i ] - s) / A [ i ][ i ])
+    return xnew
+
+def diff(x, xnew):
+    s = float(0)
+    for i in range(0, n):
+        s += float((x[i] - xnew[i]) ** 2)
+    return float(math.sqrt(s))
+
+def solve (A , f ):
+    xnew = np.zeros(n)
+    eps = 0.01
+    while True:
+        x = np.array(xnew)
+        xnew = seidel (A , f , x )
+        if(diff(x, xnew) < eps):
+            break
+    return x
+
+A = np.random.rand(n, n)
+f = np.random.rand(n)
+for i in range(n):
+    for j in range(n):
+        if i != j:
+            A[i][i] += A[i][j]
+
+print("\nMy algorithm:\n")
+my_time = time.time()
+for i in solve(A, f):
+    print(i)
+my_time = time.time() - my_time
+print("\nLinalg:\n")
+linalg_time = time.time()
+for i in np.linalg.solve(A, f):
+    print(i)
+linalg_time = time.time() - linalg_time
+
+print("\nMy time:", my_time)
+print("Linalg time:", linalg_time)
